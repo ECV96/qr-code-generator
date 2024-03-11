0ee64eac59d1334a59af403bf463746d4c979ffb
ECV96 <cantuvemanuel@gmail.com>
2024-03-10 19:31:28 -0600

    Swagger configuration done

---
+++ app/docs/swagger.js
@@ -0,0 +1,58 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerDefinition = {
+    openapi: "3.0.0",
+    info: {
+        title: "Documentation QR Code Generator",
+        version: "1.0.0"
+    },
+    servers: [
+        {
+            url: "http://localhost:3000"
+        },
+    ],
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: "http",
+                scheme: "bearer"
+            },
+        },
+        schemas: {
+            userRegistration: {
+                type: "object",
+                required: ["name", "email", "password"],
+                properties: {
+                    name: {
+                        type: "string"
+                    },
+                    email: {
+                        type: "string"
+                    },
+                    password: {
+                        type: "string"
+                    },
+                },
+            },
+            userLogin: {
+                type: "object",
+                required: ["email", "password"],
+                properties: {
+                    email: {
+                        type: "string"
+                    },
+                    password: {
+                        type: string
+                    },
+                },
+            },
+        },
+    },
+}
+
+const swaggerOptions = {
+    swaggerDefinition,
+    apis: ["./app/routes/*.js"]
+}
+
+export default swaggerJSDoc(swaggerOptions)
